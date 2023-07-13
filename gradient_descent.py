475696e0043506dd845a1db5024f20dbd6b03c2d
Russ <72242656+Russ741@users.noreply.github.com>
2023-07-12 21:23:24 -0700

    Add cost functions and test.

--- gradient_descent.py
+++ gradient_descent.py
@@ -13,3 +13,15 @@ def f_no_vector(w, b, x):
 def f_vector(w, b, x):
     return numpy.dot(x, w) + b
 
+# returns a scalar representing the cost (mean of squares of errors)
+def j_no_vector(y_hat, y):
+    cost = 0
+    m = len(y)
+    for i in range(m):
+        cost += (y_hat[i] - y[i]) ** 2
+    cost *= 1 / (2 * m)
+    return cost
+
+def j_vector(y_hat, y):
+    return 1 / (2 * len(y)) * numpy.sum(numpy.square(numpy.subtract(y_hat, y)))
+
--- gradient_descent_test.py
+++ gradient_descent_test.py
@@ -2,7 +2,7 @@
 # pytest gradient_descent.py
 
 from numpy.testing import assert_array_equal
-from gradient_descent import f_vector, f_no_vector
+from gradient_descent import f_vector, f_no_vector, j_no_vector, j_vector
 
 def test_f():
     w = [2, 3]
@@ -12,3 +12,12 @@ def test_f():
     expected = [50, 7, 49]
     assert_array_equal(f_no_vector(w, b, x), expected)
     assert_array_equal(f_vector(w, b, x), expected)
+
+def test_j():
+    y_hat = [7, 3, 8]
+    y = [9, 3, 5]
+
+    expected = 13 / 6
+    assert j_no_vector(y_hat, y) == expected
+    assert j_vector(y_hat, y) == expected
+
